a8f32ee00146b1ff646022fa7746113830bb2ef7
pengju.xu <434431984@qq.com>
2018-03-24 14:46:09 +0800

    college

---
+++ src/main/java/com/college/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.college.controller;
+
+/**
+ * @author Milo on 2018/3/24.
+ * @description
+ */
+public class UserController {
+}
---
+++ src/main/java/com/college/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.college.dao;
+
+/**
+ * @author Milo on 2018/3/24.
+ * @description
+ */
+public interface UserDao {
+}
