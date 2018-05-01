032c459384bfe32e3bae826d87c78a09e454512a
pengju.xu <434431984@qq.com>
2018-05-01 16:43:20 +0800

    step 12

--- src/main/java/com/college/controller/SigninController.java
+++ src/main/java/com/college/controller/SigninController.java
@@ -5,13 +5,18 @@ import com.college.contants.Path;
 import com.college.entity.Signin;
 import com.college.entity.User;
 import com.college.model.Resp;
+import com.college.model.UserVo;
 import com.college.service.SigninService;
 import com.college.service.UserService;
 import com.github.pagehelper.Page;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -63,16 +68,24 @@ public class SigninController extends BaseController {
                                        @RequestParam(value = "updateTime", required = false) Integer updateTime
     ) {
         Map<String, Object> params = Maps.newHashMap();
-        params.put("userId",userId);
-        params.put("status",1);
+        Integer currentId = getCurrentId();
+        User user = userService.get(currentId.longValue());
+        if (!user.getAdministrator()) {
+            params.put("userId", userId);
+        } else {
+            params.put("userId", currentId);
+        }
+        params.put("status", 1);
         Page<Signin> page = signinService.searchPageList(pageNum, pageSize, params);
         Map<String, Object> resultMap = Maps.newHashMap();
         List<Signin> singins = page.getResult();
         ArrayList<Object> results = Lists.newArrayList();
-        for (Signin signin : singins){
-            User user = userService.get(signin.getUserId().longValue());
-            String realName = user.getRealName();
-            signin.setUserName(realName);
+        for (Signin signin : singins) {
+            User signUser = userService.get(signin.getUserId().longValue());
+            if (null != signUser) {
+                String realName = signUser.getRealName();
+                signin.setUserName(realName);
+            }
             results.add(signin);
         }
         logger.info(" SigninController -->  pageResult :{}", results);
@@ -83,31 +96,51 @@ public class SigninController extends BaseController {
         resultMap.put("pagesize", page.getPageSize());
         resultMap.put("pagenum", page.getPageNum());
         resultMap.put("total", page.getTotal());
+        UserVo userVo = new UserVo();
+        BeanUtils.copyProperties(user,userVo);
+        resultMap.put("user", userVo);
         return resultMap;
     }
 
     /**
      * insert
      *
-     * @param userId     用户 ID
-     * @param status     0: 正常 1:删除
-     * @param createTime 创建时间
-     * @param updateTime 更新时间
      * @return
      */
     @RequestMapping(value = Path.SIGNIN_ADD)
     @ResponseBody
-    public Resp add(
-            @RequestParam(value = "userId", required = false) Integer userId,
-            @RequestParam(value = "status", required = false) boolean status,
-            @RequestParam(value = "createTime", required = false) java.util.Date createTime,
-            @RequestParam(value = "updateTime", required = false) java.util.Date updateTime) {
+    public Resp add() {
+        Integer currentId = getCurrentId();
+        logger.info("currentId :{}", currentId);
+        if (signInToday(currentId)) {
+            return Resp.error(AppCode._10011);
+        }
         Signin signin = new Signin();
-        signin.setUserId(userId);
-        signin.setStatus(status);
+        signin.setUserId(currentId);
         Integer id = signinService.insert(signin);
         return null != id ? Resp.success(id) : Resp.error(AppCode._10003);
     }
+
+    //是否能够签到
+    private boolean signInToday(Integer userId) {
+        DateTime dateTime = new DateTime();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+        String endTime = dateTime.toString(dateTimeFormatter) + " 23:59:59";
+        String startTime = dateTime.toString(dateTimeFormatter) + " 00:00:00";
+        Map<String, Object> params = Maps.newHashMap();
+        logger.info(" startTime :{}", startTime);
+        logger.info(" endTime :{}", endTime);
+        params.put("startTime", startTime);
+        params.put("endTime", endTime);
+        params.put("userId", userId);
+        params.put("status", true);
+        List<Signin> listByParams = signinService.findListByParams(params);
+        if (listByParams.size() == 0) {
+            return false;
+        }
+        return true;
+    }
+
 
     /**
      * update
@@ -123,7 +156,7 @@ public class SigninController extends BaseController {
     ) {
         Signin signin = new Signin();
         signin.setId(id);
-        logger.info("status :{}",status);
+        logger.info("status :{}", status);
         signin.setStatus(status);
         signinService.update(signin);
         return Resp.success();
