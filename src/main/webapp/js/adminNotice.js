6b20833de7b0560eb1549ff987318df7de4bd995
liude123 <18301575581@163.com>
2018-05-21 13:56:28 +0800

    step14

--- src/main/webapp/js/adminNotice.js
+++ src/main/webapp/js/adminNotice.js
@@ -1,169 +1,31 @@
-if($("#editor1").attr("id")!=undefined) {
-var ue1 = UE.getEditor('editor1',{
-    toolbars: [
-        ['source', 'undo', 'redo', 'bold','italic','underline','strikethrough',
-            'forecolor','lineheight','paragraph','fontfamily','fontsize','justifyleft','justifyright',
-            'justifycenter','justifyjustify','simpleupload','unlink','link']
-    ],
-    initialFrameWidth:1000,  //初始化编辑器宽度,默认1000
-    initialFrameHeight:300,  //初始化编辑器高度,默认320
-});
+var AdminNoticeObject={
+    selectChange:function () {
+        var index=$(".se").find("option:selected").index()+1;
+        $(".newsBox"+index).show().siblings("div").hide();
+
+    },
+    //单图上传  待调试
+    imgUpLoad:function (fileId, ImgId) {
+        console.log(fileId,ImgId)
+        $.ajaxFileUpload({
+            url :  urlT+ "/api/file/upload.json",
+            type : 'post',
+            secureuri : false, // 一般设置为false
+            fileElementId : fileId, // 上传文件的id、name属性名
+            dataType : 'application/json',
+            async : true,
+            success : function(data, status) {
+                /*console.log(typeof data)
+                console.log(data,status)*/
+                if(status=="success"){
+                    var srcUrl=data.substring(59,data.length-6);
+                    $("#" + ImgId).attr("src", srcUrl);
+                }
+            },
+
+        });
+    },
+    imgSave:function () {
+
+    },
 }
-if($("#editor2").attr("id")!=undefined) {
-    var ue2 = UE.getEditor('editor2', {
-        toolbars: [
-            ['source', 'undo', 'redo', 'bold', 'italic', 'underline', 'strikethrough',
-                'forecolor', 'lineheight', 'paragraph', 'fontfamily', 'fontsize', 'justifyleft', 'justifyright',
-                'justifycenter', 'justifyjustify', 'simpleupload', 'unlink', 'link']
-        ],
-        initialFrameWidth: 1000,  //初始化编辑器宽度,默认1000
-        initialFrameHeight: 300,  //初始化编辑器高度,默认320
-
-    });
-}
-if($("#editor3").attr("id")!=undefined) {
-    var ue3 = UE.getEditor('editor3', {
-        toolbars: [
-            ['source', 'undo', 'redo', 'bold', 'italic', 'underline', 'strikethrough',
-                'forecolor', 'lineheight', 'paragraph', 'fontfamily', 'fontsize', 'justifyleft', 'justifyright',
-                'justifycenter', 'justifyjustify', 'simpleupload', 'unlink', 'link']
-        ],
-        initialFrameWidth: 1000,  //初始化编辑器宽度,默认1000
-        initialFrameHeight: 300,  //初始化编辑器高度,默认320
-
-    });
-}
-
-if($("#editor4").attr("id")!=undefined) {
-
-    var ue4 = UE.getEditor('editor4', {
-        toolbars: [
-            ['source', 'undo', 'redo', 'bold', 'italic', 'underline', 'strikethrough',
-                'forecolor', 'lineheight', 'paragraph', 'fontfamily', 'fontsize', 'justifyleft', 'justifyright',
-                'justifycenter', 'justifyjustify', 'simpleupload', 'unlink', 'link']
-        ],
-        initialFrameWidth: 1000,  //初始化编辑器宽度,默认1000
-        initialFrameHeight: 300,  //初始化编辑器高度,默认320
-
-    });
-}
-if($("#editor5").attr("id")!=undefined) {
-    var ue5 = UE.getEditor('editor5', {
-        toolbars: [
-            ['source', 'undo', 'redo', 'bold', 'italic', 'underline', 'strikethrough',
-                'forecolor', 'lineheight', 'paragraph', 'fontfamily', 'fontsize', 'justifyleft', 'justifyright',
-                'justifycenter', 'justifyjustify', 'simpleupload', 'unlink', 'link']
-        ],
-        initialFrameWidth: 1000,  //初始化编辑器宽度,默认1000
-        initialFrameHeight: 300,  //初始化编辑器高度,默认320
-
-    });
-}
-if($("#editor6").attr("id")!=undefined) {
-    var ue6 = UE.getEditor('editor6', {
-        toolbars: [
-            ['source', 'undo', 'redo', 'bold', 'italic', 'underline', 'strikethrough',
-                'forecolor', 'lineheight', 'paragraph', 'fontfamily', 'fontsize', 'justifyleft', 'justifyright',
-                'justifycenter', 'justifyjustify', 'simpleupload', 'unlink', 'link']
-        ],
-        initialFrameWidth: 1000,  //初始化编辑器宽度,默认1000
-        initialFrameHeight: 300,  //初始化编辑器高度,默认320
-
-    });
-}
-if($("#editor7").attr("id")!=undefined) {
-    var ue7 = UE.getEditor('editor7', {
-        toolbars: [
-            ['source', 'undo', 'redo', 'bold', 'italic', 'underline', 'strikethrough',
-                'forecolor', 'lineheight', 'paragraph', 'fontfamily', 'fontsize', 'justifyleft', 'justifyright',
-                'justifycenter', 'justifyjustify', 'simpleupload', 'unlink', 'link']
-        ],
-        initialFrameWidth: 1000,  //初始化编辑器宽度,默认1000
-        initialFrameHeight: 300,  //初始化编辑器高度,默认320
-
-    });
-}
-if($("#editor8").attr("id")!=undefined) {
-    var ue8 = UE.getEditor('editor8', {
-        toolbars: [
-            ['source', 'undo', 'redo', 'bold', 'italic', 'underline', 'strikethrough',
-                'forecolor', 'lineheight', 'paragraph', 'fontfamily', 'fontsize', 'justifyleft', 'justifyright',
-                'justifycenter', 'justifyjustify', 'simpleupload', 'unlink', 'link']
-        ],
-        initialFrameWidth: 1000,  //初始化编辑器宽度,默认1000
-        initialFrameHeight: 300,  //初始化编辑器高度,默认320
-
-    });
-}
-if($("#editor9").attr("id")!=undefined) {
-    var ue9 = UE.getEditor('editor9', {
-        toolbars: [
-            ['source', 'undo', 'redo', 'bold', 'italic', 'underline', 'strikethrough',
-                'forecolor', 'lineheight', 'paragraph', 'fontfamily', 'fontsize', 'justifyleft', 'justifyright',
-                'justifycenter', 'justifyjustify', 'simpleupload', 'unlink', 'link']
-        ],
-        initialFrameWidth: 1000,  //初始化编辑器宽度,默认1000
-        initialFrameHeight: 300,  //初始化编辑器高度,默认320
-
-    });
-}
-if($("#editor10").attr("id")!=undefined) {
-    var ue10 = UE.getEditor('editor10', {
-        toolbars: [
-            ['source', 'undo', 'redo', 'bold', 'italic', 'underline', 'strikethrough',
-                'forecolor', 'lineheight', 'paragraph', 'fontfamily', 'fontsize', 'justifyleft', 'justifyright',
-                'justifycenter', 'justifyjustify', 'simpleupload', 'unlink', 'link']
-        ],
-        initialFrameWidth: 1000,  //初始化编辑器宽度,默认1000
-        initialFrameHeight: 300,  //初始化编辑器高度,默认320
-
-    });
-}
-if($("#editor11").attr("id")!=undefined) {
-    var ue11 = UE.getEditor('editor11', {
-        toolbars: [
-            ['source', 'undo', 'redo', 'bold', 'italic', 'underline', 'strikethrough',
-                'forecolor', 'lineheight', 'paragraph', 'fontfamily', 'fontsize', 'justifyleft', 'justifyright',
-                'justifycenter', 'justifyjustify', 'simpleupload', 'unlink', 'link']
-        ],
-        initialFrameWidth: 1000,  //初始化编辑器宽度,默认1000
-        initialFrameHeight: 300,  //初始化编辑器高度,默认320
-
-    });
-}
-
-$(".se").change(function () {
-    var index=$(this).find("option:selected").index()+1
-    $(".newsBox"+index).show().siblings("div").hide()
-})
-
-//单图上传
-function imgUpLoad(fileId, ImgId) {
-    $.ajaxFileUpload({
-        url :"",
-        type : 'post',
-        secureuri : false, // 一般设置为false
-        fileElementId : fileId, // 上传文件的id、name属性名
-        dataType : 'text',
-        success : function(data, status) {
-            var obj = eval("(" + data + ")");
-            if (!obj.success) {
-                alert(obj.msg);
-            } else {
-                $("#" + ImgId).attr("src", obj.msg);
-                imgSave();// 实时保存
-            }
-        },
-        error : function(data, status, e) {
-           alert("上传失败");
-        }
-    });
-}
-function imgSave() {
-
-}
-//保存
-$(".saveBtn").eq(0).click(function () {
-    alert(ue1.getContent())
-})
-
